1664a34f3e3b773893f32efcb6d8950ca41d0655
Ryan <ryansilvadeoliveira53@gmail.com>
2026-01-09 00:35:30 -0300

    estilização artico

--- web-artic/src/containers/Home/sections/Artico.jsx
+++ web-artic/src/containers/Home/sections/Artico.jsx
@@ -4,14 +4,22 @@ function Artico() {
     return (
         <section className='artico'>
             <div className='container'>
-                <div className='info'>
-                    <div className='title'>Ártico</div>
-                    <div className='texto'>O Ártico, ou Região Ártica, é geralmente definido como a região do Hemisfério Norte circunscrita pela linha onde a temperatura média do mês mais quente é inferior a 10 ℃. A linha isotérmica delimitando esta região coincide aproximadamente com a linha das árvores ártica.</div>
+                <div className='title'>Ártico</div>
+                <article className='info'>
 
-                    <div className='imageArtic'>
-                    <img src='/Artico.png' alt=''></img>
+                    <div className='textArea'>
+                    <p className='texto'>O Ártico, ou Região Ártica, é geralmente definido como a região do Hemisfério Norte circunscrita pela linha onde a temperatura média do mês mais quente é inferior a 10 ℃. A linha isotérmica delimitando esta região coincide aproximadamente com a linha das árvores ártica.</p>
+
+                    <a className='leia-mais' href='https://pt.wikipedia.org/wiki/%C3%81rtico' target='_blank' rel="noopener noreferrer">
+                    Leia mais <span class="material-symbols-outlined">arrow_right_alt</span>
+                    </a>
                     </div>
-                </div>
+
+                    <figure className='imageArtic'>
+                    <img src='/Artico.png' alt='geoMundo' />
+                    </figure>
+
+                </article>
             </div>
         </section>
     )
